4590e1cb7b5bfb49559a722a3409bfafcf11e970
MaggieChafee <maggie.chafee@gmail.com>
2024-01-15 11:08:02 -0600

    Phase 2 completed

--- Program.cs
+++ Program.cs
@@ -3,8 +3,36 @@ Can you guess the secret number?
 ";
 
 string guessPrompt = "What do you think the secret number is?";
+int secretNumber = 42;
 
 Console.WriteLine(greeting);
 Console.WriteLine(guessPrompt);
-string userInput = Console.ReadLine();
-Console.WriteLine(userInput);
+string userInput = null;
+while (userInput == null)
+{
+    try
+    {
+        int intUserInput = Convert.ToInt32(Console.ReadLine());
+        if (intUserInput == secretNumber)
+        {
+            Console.WriteLine("Congratulations! You guessed the secret number.");
+        }
+        else if (intUserInput != secretNumber)
+        {
+            Console.WriteLine("Looks like you didn't guess the secret number. Better luck next time!");
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please type only integars!");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+        Console.WriteLine("Try again.");
+    }
+}
+Console.WriteLine("Goodbye.");
+
+
+
