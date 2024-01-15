387e679f3d180e2e3c05d853d169e626479f2852
MaggieChafee <maggie.chafee@gmail.com>
2024-01-15 10:52:46 -0600

    Phase 1 completed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string greeting = @"    **** Welcome to Guess that Number! ****
+Can you guess the secret number?
+";
+
+string guessPrompt = "What do you think the secret number is?";
+
+Console.WriteLine(greeting);
+Console.WriteLine(guessPrompt);
+string userInput = Console.ReadLine();
+Console.WriteLine(userInput);
